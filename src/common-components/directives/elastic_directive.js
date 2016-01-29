e638309071c5b65717d0e33c6d86c883520b87ad
catarak <ctarakajian@gmail.com>
2016-01-28 22:51:43 -0500

    change input to text area, which doesn't quite work yet

---
+++ src/common-components/directives/elastic_directive.js
@@ -0,0 +1,19 @@
+var elastic = angular.module('elastic', []);
+
+elastic.directive('elastic', [
+    '$timeout',
+    function($timeout) {
+        return {
+            restrict: 'A',
+            link: function($scope, element) {
+                $scope.initialHeight = $scope.initialHeight || element[0].style.height;
+                var resize = function() {
+                    element[0].style.height = $scope.initialHeight;
+                    element[0].style.height = "" + element[0].scrollHeight + "px";
+                };
+                element.on("blur keyup change", resize);
+                $timeout(resize, 0);
+            }
+        };
+    }
+]);
