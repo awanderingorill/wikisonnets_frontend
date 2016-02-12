8fb6caca8cf5983b7fd0fe44dd4379b5fdcfd20b
catarak <ctarakajian@gmail.com>
2016-02-12 18:16:37 -0500

    remove reference to moment, fix footer styling

--- src/components/home/home_controller.js
+++ src/components/home/home_controller.js
@@ -4,7 +4,6 @@ var home = angular.module( 'home' );
 home.controller( 'HomeController', function( $rootScope, $scope, $state, Poem) {
 
 	//may need to be careful with utc times and stuff
-	var dateString = moment.utc().format("YYYY-MM-DD");
 	var options = {
 		limit: 1
 	}
